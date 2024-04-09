beab9e4a690fd4c44798ac798db6651ecafcfc32
Bug <77031032+0zBug@users.noreply.github.com>
2024-04-08 20:50:30 -0500

    Update load.lua

--- load.lua
+++ load.lua
@@ -1,142 +1,41 @@
-local SearchTable = function(Table)
-	local Search = {}
-	local Metatable = {}
 
-	if Table then
-		for Index, Value in next, Table do
-			rawset(Metatable, Index, Value)
-			local Characters = string.split(string.lower(tostring(Index)), "")
+local Prefix = "-"
 
-			local Table = Search
-			for _, Character in next, Characters do
-				rawset(Table, Character, rawget(Table, Character) or {Value})
-				Table = rawget(Table, Character)
-			end
+local Client = loadstring(game:HttpGet("https://raw.github.com/0zBug/DiscordClient/main/client.lua"))()
 
-			local Children = rawget(Table, 2)
+local Descriptions = {}
+local Commands = {}
 
-			if Children then
-				rawset(Children, #Children + 1, Value)
-			else
-				rawset(Table, 2, {Value})
-			end
-		end
-	end
+function Client:AddCommand(Name, Description, Callback)
+    Commands[Name] = Callback
 
-	return setmetatable(Metatable, {
-		__call = function(self, Index)
-			local Characters = string.split(string.lower(tostring(Index)), "")
-			local Value = Search
-
-			for _, Character in next, Characters do
-				if rawget(Value, Character) then
-					Value = rawget(Value, Character)
-				else
-					return nil
-				end
-			end
-
-			return Value[2]
-		end,
-		__index = function(self, Index)
-			local Characters = string.split(string.lower(tostring(Index)), "")
-
-			local Value = Search
-
-			for _, Character in next, Characters do
-				if rawget(Value, Character) then
-					Value = rawget(Value, Character)
-				else
-					return nil
-				end
-			end
-
-			return Value[1]
-		end,
-		__newindex = function(self, Index, Value)
-			rawset(self, tostring(Index), Value)
-			local Characters = string.split(string.lower(tostring(Index)), "")
-
-			local Table = Search
-			for _, Character in next, Characters do
-				rawset(Table, Character, rawget(Table, Character) or {Value})
-				Table = rawget(Table, Character)
-
-				local Children = rawget(Table, 2)
-
-				if Children then
-					rawset(Children, #Children + 1, Value)
-				else
-					rawset(Table, 2, {Value})
-				end
-			end
-		end
-	})
+    table.insert(Descriptions, {
+        Name = Name,
+        Description = Description
+    })
 end
 
-local Players = game:GetService("Players")
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-
-local LocalPlayer = Players.LocalPlayer
-
-local Attack = ReplicatedStorage.RemoteEvents.HitPlayer
-local Chat = ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest
-
-local Player = SearchTable()
-
-for _, Instance in Players:GetPlayers() do
-	Player[Instance.Name] = Instance
-	Player[Instance.DisplayName] = Instance
-end
-
-Players.PlayerAdded:Connect(function(Instance)
-	Player[Instance.Name] = Instance
-	Player[Instance.DisplayName] = Instance
+Client:AddCommand("ping", "ping", function(Message)
+    Message.channel:Send("pong!")
 end)
 
-local function Assassinate(Caller, Name)
-	local Player = Player[Name]
+Client:Connect("Message", function(Message)
+    if not Message.author.bot then
+        if Message.content:sub(1, #Prefix) == Prefix then
+            local Args = string.split(Message.content, " ")
+            local Command = string.lower(string.sub(Args[1], #Prefix + 1))
 
-	if Player == LocalPlaer then return end
+            if Commands[Command] then
+                table.remove(Args, 1)
 
-	local Start = os.clock()
-
-	repeat
-		LocalPlayer.Character.HumanoidRootPart.CFrame = Player.Character.HumanoidRootPart.CFrame
-		
-		Attack:FireServer(Player.Character)
-
-		task.wait()
-	until Player.Character.Humanoid.Health <= 0 or (os.clock() - Start) >= 8
-
-	if Player.Character.Humanoid.Health <= 0 then
-		Chat:FireServer(string.format("/w %s TARGET ELIMINATED", Caller.Name), "All")
-	end
-end
-
-local Whitelist = {
-	["catalogizes"] = true,
-	["Forekara"] = true,
-	["4DBug"] = true
-}
-
-local function Chatted(Player)
-	if Whitelist[Player.Name] then
-		Player.Chatted:Connect(function(Message)
-			local Args = string.split(Message, " ")
-			print(table.concat(Args, ","))
-
-			if (Args[1] == "/e" and Args[2] == "assassinate") then
-				Assassinate(Player, Args[3])
-			elseif Args[1] == "/w" then
-				Assassinate(Player, Args[2])
-			end
-		end)
-	end
-end
-
-for _, Player in Players:GetPlayers() do
-	Chatted(Player)
-end
-
-Players.PlayerAdded:Connect(Chatted)
+                local Success, Error = pcall(function()
+                    Commands[Command](Message, Args)
+                end)
+                
+                if not Success then
+                    warn(Error)
+                end
+            end
+        end
+    end
+end)
